65340a0432bba3ef3890e82dda3d24c3ea8f480e
eeeeeeeesw <162020434+eeeeeeeesw@users.noreply.github.com>
2024-03-10 12:43:10 +0000

    Update bluefox script.lua

--- bluefox script.lua
+++ bluefox script.lua
@@ -29,7 +29,7 @@ end
 
 --
 Library=loadstring(game:HttpGet("https://raw.githubusercontent.com/eeeeeeeesw/Bluefox/main/script%20test"))()
-Window = Library.Main("Bluefox Script","RightShift")
+Window = Library.Main("Bluefox Script 1.12.3","RightShift")
 
 _G.Rainbowwings = false 
 local Tab = Window.NewTab("Settings")
