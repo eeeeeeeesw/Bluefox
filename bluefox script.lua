f9c4ec616c8f59e5acfb38c3e19591427e7e7446
eeeeeeeesw <162020434+eeeeeeeesw@users.noreply.github.com>
2024-03-14 22:05:12 +0000

    Update bluefox script.lua

--- bluefox script.lua
+++ bluefox script.lua
@@ -1,6 +1,6 @@
 
 game:GetService('StarterGui'):SetCore("SendNotification", {
-	Title = 'Bluefox script 1.12.31';
+	Title = 'Bluefox script 1.12.31a';
 	Text = 'Hope you enjoying the script!';
 	Icon = "rbxassetid://14899955386";
 	Duration = 3.4028235e+38; 
@@ -1455,8 +1455,8 @@ createButton("VIW Den Forest 2", 519.791199, 48.2600021, -47.4920311, 0.60065746
 createButton("VIW Den Redwood", 532.389893, 87.6499786, 476.836426, -0.24057126, 0.97063148, -0.97063148)
 createButton("VIW Den Redwood 2", 557.94342, 64.1299896, 619.453613, 0.996085644, -0.0883934423, 0.0883934423)
 createButton("VIW Den Redwood 3", 266.536804, 45.2700043, 574.910095, -0.373445153, 0.927652299, -0.927652299)
-local tab = windoww.NewTab("testings")
-local Section = Tab.NewSection("feature testing")
+local Tab = Window.NewTab("testes")
+local Section = Tab.NewSection("stuff in testing")
 local hairfTextBox = Section.NewTextBox('HairF', function(self, value)
     hairfValue = value
 end)
